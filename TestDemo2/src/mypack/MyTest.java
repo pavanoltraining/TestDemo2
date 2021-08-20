14da1ebf0ae0bf492eb69c1695706dd20e024ff1
Pavan <pavanoltraining@gmail.com>
2021-08-20 07:18:54 +0530

    this is my commit 2

--- TestDemo2/src/mypack/MyTest.java
+++ TestDemo2/src/mypack/MyTest.java
@@ -4,6 +4,8 @@ public class MyTest {
 
 	public static void main(String[] args) {
 	System.out.println("This is test demo....");
+	System.out.println("This is test demo....");
+	System.out.println("This is test demo....");
 
 	}
 
